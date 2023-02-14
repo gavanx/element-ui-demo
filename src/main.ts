4b389ef6043585170eda483dc9758b1121bac6e3
gavan <guoming3@xiaomi.com>
2023-02-14 10:16:50 +0800

    raw select.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
+
+Vue.use(ElementUI);
 Vue.use(PiniaVuePlugin)
 
 new Vue({
