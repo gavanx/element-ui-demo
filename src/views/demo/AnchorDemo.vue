1ab21d238210bdf5b10929319235dab1adf352c5
gavan <guoming3@xiaomi.com>
2023-03-16 19:06:01 +0800

    scroll top.

--- src/views/demo/AnchorDemo.vue
+++ src/views/demo/AnchorDemo.vue
@@ -4,6 +4,7 @@
       <h2><a href="#one">one</a></h2>
       <h2><a href="#two">two</a></h2>
       <h2><a href="#three">three</a></h2>
+      <el-button @click="handleScrollTop">scrollTop</el-button>
     </div>
     <div style="height: 70vh; background: red" id="one">one</div>
     <div style="height: 70vh; background: blue" id="two">two</div>
@@ -13,9 +14,15 @@
 </template>
 <script>
 import Backtop from '../components/Backtop.vue';
+import scrollTop from '../components/scrollTop';
 export default {
   components: {
     Backtop,
+  },
+  methods: {
+    handleScrollTop() {
+      scrollTop(document.documentElement);
+    },
   },
 };
 </script>
